772dea928f7816d698bb004ec725c46a631c00cb
Colin Unger <lockshaw@lockshaw.net>
2024-06-25 20:33:14 -0700

    Make typing-extensions available at runtime

--- proj.nix
+++ proj.nix
@@ -1,11 +1,13 @@
-{ buildPythonApplication, python3Packages, ... }:
+{ buildPythonApplication
+, python3Packages
+}:
 
 buildPythonApplication {
   pname = "proj";
   version = "0.0.1";
   src = ./.;
 
-  buildInputs = [
+  propagatedBuildInputs = [
     python3Packages.typing-extensions
   ];
 }
