c9f8e044334fd07c19d371e8ca031cf990ca11e4
Colin Unger <lockshaw@lockshaw.net>
2025-05-01 00:05:37 -0700

    Fix typo

--- proj/failure.py
+++ proj/failure.py
@@ -3,7 +3,7 @@ import logging
 
 _l = logging.getLogger(__name__)
 
-def fail_without_error(error_core: int = 1) -> None:
+def fail_without_error(error_code: int = 1) -> None:
     sys.exit(error_code)
 
 def fail_with_error(err: str, error_code: int = 1) -> None:
